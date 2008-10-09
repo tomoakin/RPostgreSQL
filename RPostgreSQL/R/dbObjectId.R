e2868118b8866cb1c0f861c403d7d549d0ef165c
Dirk Eddelbuettel <edd@debian.org>
2008-10-09 00:56:45 +0000

    no code changes, but reindentation / re-formatting to the R Core / ESS default (cf section 5 of R Internals manual)

--- RPostgreSQL/R/dbObjectId.R
+++ RPostgreSQL/R/dbObjectId.R
@@ -1,4 +1,5 @@
-## Class: dbObjectId		Last Modified:
+## Class: dbObjectId
+## Last Modified: $Date#
 
 ## This package was developed as a part of Summer of Code program organized by Google.
 ## Thanks to David A. James & Saikat DebRoy, the authors of RMySQL package.
@@ -10,50 +11,48 @@
 ## This mixin helper class is NOT part of the database interface definition,
 ## but it is extended by the Oracle, MySQL,PostgreSQL and SQLite implementations
 ## to allow us to conviniently (and portably)
-## implement all database foreign objects methods (i.e., methods for show(), 
-## print() format() the dbManger, dbConnection, dbResultSet, etc.) 
-## A dbObjectId is an  identifier into an actual remote database objects.  
-## This class and its derived classes <driver-manager>Object need to 
+## implement all database foreign objects methods (i.e., methods for show(),
+## print() format() the dbManger, dbConnection, dbResultSet, etc.)
+## A dbObjectId is an  identifier into an actual remote database objects.
+## This class and its derived classes <driver-manager>Object need to
 ## be VIRTUAL to avoid coercion (green book, p.293) during method dispatching.
 
 setClass("dbObjectId", representation(Id = "integer", "VIRTUAL"))
 
-## coercion methods 
-setAs("dbObjectId", "integer", 
-   def = function(from) as(slot(from,"Id"), "integer")
-)
+## coercion methods
+setAs("dbObjectId", "integer",
+      def = function(from) as(slot(from,"Id"), "integer")
+      )
 setAs("dbObjectId", "numeric",
-   def = function(from) as(slot(from, "Id"), "integer")
-)
+      def = function(from) as(slot(from, "Id"), "integer")
+      )
 setAs("dbObjectId", "character",
-   def = function(from) as(slot(from, "Id"), "character")
-)   
+      def = function(from) as(slot(from, "Id"), "character")
+      )
 
 ## formating, showing, printing,...
-setMethod("format", "dbObjectId", 
-   def = function(x, ...) {
-      paste("(", paste(as(x, "integer"), collapse=","), ")", sep="")
-   },
-   valueClass = "character"
-)
+setMethod("format", "dbObjectId",
+          def = function(x, ...) {
+              paste("(", paste(as(x, "integer"), collapse=","), ")", sep="")
+          },
+          valueClass = "character"
+          )
 
 setMethod("show", "dbObjectId", def = function(object) print(object))
 
 setMethod("print", "dbObjectId",
-   def = function(x, ...){
-      expired <- if(isIdCurrent(x)) "" else "Expired "
-      str <- paste("<", expired, class(x), ":", format(x), ">", sep="")
-      cat(str, "\n")
-      invisible(NULL)
-   }
-)
+          def = function(x, ...){
+              expired <- if(isIdCurrent(x)) "" else "Expired "
+              str <- paste("<", expired, class(x), ":", format(x), ">", sep="")
+              cat(str, "\n")
+              invisible(NULL)
+          }
+          )
 
-"isIdCurrent" <- 
-function(obj)
 ## verify that obj refers to a currently open/loaded database
-{ 
-   obj <- as(obj, "integer")
-   .Call("RS_DBI_validHandle", obj, PACKAGE = .PostgreSQLPkgName)
+isIdCurrent <- function(obj) {
+    obj <- as(obj, "integer")
+    .Call("RS_DBI_validHandle", obj, PACKAGE = .PostgreSQLPkgName)
 }
 
- 
+
