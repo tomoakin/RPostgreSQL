bff090c069a20a0c987221477dac468797e6da42
Dirk Eddelbuettel <edd@debian.org>
2008-10-12 18:04:50 +0000

    only run code inside of tests/loadDriverAndConnect.R if a specific env.var is set as we cannot assume everybody has Postgres running

--- RPostgreSQL/tests/loadDriverAndConnect.R
+++ RPostgreSQL/tests/loadDriverAndConnect.R
@@ -9,20 +9,24 @@
 ##
 ## Dirk Eddelbuettel, 02 July 2008
 
-## try to load our module and abort if this fails
-stopifnot(require(RPostgreSQL))
+## only run this if this env.var is set correctly
+if (Sys.getenv("HAVE_POSTGRES_RUNNING") == "yes") {
 
-## load the PostgresSQL driver
-drv <- dbDriver("PostgreSQL")
-# can't print result as it contains process id which changes  print(summary(drv))
+    ## try to load our module and abort if this fails
+    stopifnot(require(RPostgreSQL))
 
-## connect to the default db
-con <- dbConnect(drv, dbname="template1")
-# -- idem  print(con)
+    ## load the PostgresSQL driver
+    drv <- dbDriver("PostgreSQL")
+    ## can't print result as it contains process id which changes  print(summary(drv))
 
-## run a simple query and show the query result
-res <- dbGetQuery(con, "select datname,encoding,datallowconn from pg_database where datname like 'template%' order by datname")
-print(res)
+    ## connect to the default db
+    con <- dbConnect(drv, dbname="template1")
+    ## -- idem  print(con)
 
-## and disconnect
-dbDisconnect(con)
+    ## run a simple query and show the query result
+    res <- dbGetQuery(con, "select datname,encoding,datallowconn from pg_database where datname like 'template%' order by datname")
+    print(res)
+
+    ## and disconnect
+    dbDisconnect(con)
+}
