0ee9adaba43001ce9d736878a3ee66a4dc1cc0c2
dirk.eddelbuettel <dirk.eddelbuettel@700bd7ec-5b50-0410-b289-319adfe7ca7c>
2008-11-02 23:00:36 +0000

    fixed another typo in env.var.

--- trunk/RPostgreSQL/tests/loadDriverAndConnect.R
+++ trunk/RPostgreSQL/tests/loadDriverAndConnect.R
@@ -24,7 +24,7 @@ if (Sys.getenv("POSTGRES_USER") != "" & Sys.getenv("POSTGRES_HOST") != "" & Sys.
                      user=Sys.getenv("POSTGRES_USER"),
                      password=Sys.getenv("POSTGRES_PASSWD"),
                      host=Sys.getenv("POSTGRES_HOST"),
-                     dbname=Sys.getenv("POSTGRES_DATABSE"))
+                     dbname=Sys.getenv("POSTGRES_DATABASE"))
 
     ## run a simple query and show the query result
     res <- dbGetQuery(con, paste("select datname,encoding,datallowconn from pg_database",
