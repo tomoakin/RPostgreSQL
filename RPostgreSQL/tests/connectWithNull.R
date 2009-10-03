0e2ce543d0139d8e1ac0157750e1350141d714cd
Dirk Eddelbuettel <edd@debian.org>
2009-10-03 22:39:48 +0000

    added new test and reference output to test for issue 2 which is now fixed

---
+++ RPostgreSQL/tests/connectWithNull.R
@@ -0,0 +1,37 @@
+
+## connectWithNull test
+##
+## test for the 'Issue 2' on the Google Code issue log
+## reported in April 2009, still open ?
+##
+## Assumes that
+##  a) PostgreSQL is running, and
+##  b) the current user can connect
+## both of which are not viable for release but suitable while we test
+##
+## Dirk Eddelbuettel, 03 Oct 2009
+
+## only run this if this env.var is set correctly
+if (Sys.getenv("POSTGRES_USER") != "" & Sys.getenv("POSTGRES_HOST") != "" & Sys.getenv("POSTGRES_DATABASE") != "") {
+
+    ## try to load our module and abort if this fails
+    stopifnot(require(RPostgreSQL))
+    stopifnot(require(datasets))
+
+    ## load the PostgresSQL driver
+    drv <- dbDriver("PostgreSQL")
+
+    ## connect to the default db -- replacing any of these with NULL will lead to
+    ## a stop() call and a return to the R prompt rather than a segfault
+    con <- dbConnect(drv,
+                     user=Sys.getenv("POSTGRES_USER"),
+                     password=Sys.getenv("POSTGRES_PASSWD"),
+                     host=Sys.getenv("POSTGRES_HOST"),
+                     dbname=Sys.getenv("POSTGRES_DATABASE"))
+
+    ## do we get here?
+    print(con)
+
+    ## and disconnect
+    dbDisconnect(con)
+}
