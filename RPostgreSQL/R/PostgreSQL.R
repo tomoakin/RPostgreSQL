c5583d7543cf2c4e5b0f8b747325fdfa671f50f6
Dirk Eddelbuettel <edd@debian.org>
2009-10-16 17:50:09 +0000

    add missing paste() in dbListTables -- thanks to João G.

--- RPostgreSQL/R/PostgreSQL.R
+++ RPostgreSQL/R/PostgreSQL.R
@@ -138,8 +138,8 @@ setMethod("summary", "PostgreSQLConnection",
 setMethod("dbListTables", "PostgreSQLConnection",
           def = function(conn, ...){
               out <- dbGetQuery(conn,
-                                "select tablename from pg_tables where schemaname !='information_schema' ",
-                                "and schemaname !='pg_catalog'", ...)
+                                paste("select tablename from pg_tables where schemaname !='information_schema'",
+                                      "and schemaname !='pg_catalog'", ...))
               if (is.null(out) || nrow(out) == 0)
                   out <- character(0)
               else
