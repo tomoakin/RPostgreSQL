a9f52bd03d25751fd3ab257111c8e899bf4fbd26
Dirk Eddelbuettel <edd@debian.org>
2009-10-19 20:59:30 +0000

    also allow for port to be passed via an environment variable

--- RPostgreSQL/tests/dataTypeTests.R
+++ RPostgreSQL/tests/dataTypeTests.R
@@ -18,7 +18,8 @@ if ((Sys.getenv("POSTGRES_USER") != "") &
                      user=Sys.getenv("POSTGRES_USER"),
                      password=Sys.getenv("POSTGRES_PASSWD"),
                      host=Sys.getenv("POSTGRES_HOST"),
-                     dbname=Sys.getenv("POSTGRES_DATABASE"))
+                     dbname=Sys.getenv("POSTGRES_DATABASE"),
+                     port=ifelse((p<-Sys.getenv("POSTGRES_PORT"))!="", p, 5432))
 
     if (dbExistsTable(con, "tempostgrestable"))
         dbRemoveTable(con, "tempostgrestable")
--- RPostgreSQL/tests/selectWithAlias.R
+++ RPostgreSQL/tests/selectWithAlias.R
@@ -26,8 +26,8 @@ if (Sys.getenv("POSTGRES_USER") != "" & Sys.getenv("POSTGRES_HOST") != "" & Sys.
                      user=Sys.getenv("POSTGRES_USER"),
                      password=Sys.getenv("POSTGRES_PASSWD"),
                      host=Sys.getenv("POSTGRES_HOST"),
-                     dbname=Sys.getenv("POSTGRES_DATABASE"))
-
+                     dbname=Sys.getenv("POSTGRES_DATABASE"),
+                     port=ifelse((p<-Sys.getenv("POSTGRES_PORT"))!="", p, 5432))
 
     if (dbExistsTable(con, "rockdata")) {
         print("Removing rockdata\n")
