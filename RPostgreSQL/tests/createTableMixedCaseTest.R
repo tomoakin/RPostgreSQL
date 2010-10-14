c2d957a61a6b30d39bf195f40acdd577e7e3c510
neilt@neiltiffin.com <neilt@neiltiffin.com@700bd7ec-5b50-0410-b289-319adfe7ca7c>
2010-10-14 14:32:57 +0000

    Add missing port.

--- RPostgreSQL/tests/createTableMixedCaseTest.R
+++ RPostgreSQL/tests/createTableMixedCaseTest.R
@@ -23,7 +23,8 @@ if (Sys.getenv("POSTGRES_USER") != "" & Sys.getenv("POSTGRES_HOST") != "" & Sys.
                      user=Sys.getenv("POSTGRES_USER"),
                      password=Sys.getenv("POSTGRES_PASSWD"),
                      host=Sys.getenv("POSTGRES_HOST"),
-                     dbname=Sys.getenv("POSTGRES_DATABASE"))
+                     dbname=Sys.getenv("POSTGRES_DATABASE"),
+                     port=Sys.getenv("POSTGRES_PORT"))
 
 
 
