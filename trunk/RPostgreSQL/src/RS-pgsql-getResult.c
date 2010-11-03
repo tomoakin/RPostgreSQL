18ed37694eca5291609f0d37d0203bfc22e34bfc
tomoakin@kenroku.kanazawa-u.ac.jp <tomoakin@kenroku.kanazawa-u.ac.jp@700bd7ec-5b50-0410-b289-319adfe7ca7c>
2010-11-03 06:05:56 +0000

    Get results after CopyIn so that error is raised when incompatible table exists for dbWriteTable(append=TRUE) (issue 26).

---
+++ trunk/RPostgreSQL/src/RS-pgsql-getResult.c
@@ -0,0 +1,83 @@
+/*
+ *    RS-pgsql-copy.c
+ */
+
+#include "RS-PostgreSQL.h"
+#define COPY_IN_BUFSIZE 8192
+
+/* adapter for PQputCopyData and PQputCopyEnd 
+   which is used in conjunction with COPY table from STDIN */
+
+/*
+ * Copies all content of the file specified with filename to the conHandle which
+ * has opened connection previously issued the COPY table from STDIN query
+ * the data is read from file sent to the database with PQputCopyData
+ * in chunks of COPY_IN_BUFSIZE.
+ * The copy ends when 0 byte could be read from the file and then the
+ * PQputCopyEnd is called to complete the copying.
+ */
+
+
+s_object *
+RS_PostgreSQL_getResult(Con_Handle * conHandle)
+{
+    S_EVALUATOR RS_DBI_connection * con;
+    S_EVALUATOR RS_DBI_resultSet * result;
+    PGconn *my_connection;
+    Res_Handle *rsHandle;
+    Sint res_id;
+ 
+    char *dyn_filename;
+    char copybuf[COPY_IN_BUFSIZE];
+    FILE* filehandle;
+    size_t len;
+    int pqretcode;
+    PGresult *my_result;
+   
+    con = RS_DBI_getConnection(conHandle);
+    my_connection = (PGconn *) con->drvConnection;
+
+    if (con->num_res > 0) {
+        res_id = (Sint) con->resultSetIds[0];
+        rsHandle = RS_DBI_asResHandle(MGR_ID(conHandle), CON_ID(conHandle), res_id);
+        result = RS_DBI_getResultSet(rsHandle);
+        if (result->completed == 0) {
+            RS_DBI_errorMessage("connection with pending rows, close resultSet before continuing", RS_DBI_ERROR);
+        }
+        else {
+            RS_PostgreSQL_closeResultSet(rsHandle);
+        }
+    }
+
+    my_result = PQgetResult(my_connection);
+    if(my_result == NULL)
+       return S_NULL_ENTRY;
+    if (strcmp(PQresultErrorMessage(my_result), "") != 0) {
+        char *errResultMsg;
+        const char *omsg;
+        size_t len;
+        omsg = PQerrorMessage(my_connection);
+        len = strlen(omsg);
+        errResultMsg = malloc(len + 80); /* 80 should be larger than the length of "could not ..."*/
+        snprintf(errResultMsg, len + 80, "could not Retrieve the result : %s", omsg);
+        RS_DBI_errorMessage(errResultMsg, RS_DBI_ERROR);
+        free(errResultMsg);
+
+        /*  Frees the storage associated with a PGresult.
+         *  void PQclear(PGresult *res);   */
+
+        PQclear(my_result);
+
+    }
+
+    /* we now create the wrapper and copy values */
+    rsHandle = RS_DBI_allocResultSet(conHandle);
+    result = RS_DBI_getResultSet(rsHandle);
+    result->drvResultSet = (void *) my_result;
+    result->rowCount = (Sint) 0;
+    result->isSelect = 0;
+    result->rowsAffected = 0;
+    result->completed = 1;
+    return rsHandle;
+}
+
