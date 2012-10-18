4d0e4e0a40b16e4cbe5ab39f040b1c3c6557bdaa
tomoakin@kenroku.kanazawa-u.ac.jp <tomoakin@kenroku.kanazawa-u.ac.jp@700bd7ec-5b50-0410-b289-319adfe7ca7c>
2012-10-18 07:16:04 +0000

    Set datestyle to ISO by default

--- RPostgreSQL/R/PostgreSQLSupport.R
+++ RPostgreSQL/R/PostgreSQLSupport.R
@@ -78,7 +78,7 @@ postgresqlDriverInfo <- function(obj, what="", ...) {
 ## while NULL means "no database".
 postgresqlNewConnection <- function(drv, user = "", password = "",
                                     host = "", dbname = "",
-                                    port = "", tty = "", options = "") {
+                                    port = "", tty = "", options = "", forceISOdate=TRUE) {
     if(!isPostgresqlIdCurrent(drv))
         stop("expired manager")
     if(is.null(user))
@@ -98,7 +98,11 @@ postgresqlNewConnection <- function(drv, user = "", password = "",
 
     drvId <- as(drv, "integer")
     conId <- .Call("RS_PostgreSQL_newConnection", drvId, con.params, PACKAGE = .PostgreSQLPkgName)
-    new("PostgreSQLConnection", Id = conId)
+    con <- new("PostgreSQLConnection", Id = conId)
+    if(forceISOdate){
+      dbGetQuery(con, "set datestyle to ISO")      
+    }
+    con
 }
 
 postgresqlCloneConnection <- function(con, ...) {
@@ -411,21 +415,14 @@ postgresqlFetch <- function(res, n=0, ...) {
 
     flds <- dbGetInfo(res)$fieldDescription[[1]]$type
     for(i in 1:length(flds)) {
-
-        ## Note: All the Date-Time datatypes in pg except date and TimeStamp were mapped to character
-
         if(flds[[i]] == 1114) {  ## 1114 corresponds to Timestamp without TZ (mapped to POSIXct class)
             rel[,i] <- as.POSIXct(rel[,i])
         } else if(flds[[i]] == 1082) {  ## 1082 corresponds to Date (mapped to Date class)
             rel[,i] <- as.Date(rel[,i])
         } else if(flds[[i]] == 1184)  {  ## 1184 corresponds to Timestamp with TimeZone
-            ## TODO: Details about time zone has been dropped.
-            ## Will try do improve it in the future
-            #rel[,i] <- as.POSIXct(rel[,i],"%Y-%m-%d %H:%M:%S")  # second arg. is tz, so this was wrong
             rel[,i] <- as.POSIXct(sub('([+-]..)$', '\\100', sub(':(..)$','\\1' ,rel[,i])), format="%Y-%m-%d %H:%M:%OS%z")
         }
     }
-
     rel
 }
 
