3cff7b7d377160ed5d1f4839856ae4ed832e77a9
tomoakin@kenroku.kanazawa-u.ac.jp <tomoakin@kenroku.kanazawa-u.ac.jp@700bd7ec-5b50-0410-b289-319adfe7ca7c>
2014-01-29 13:17:15 +0000

    Initial bytea support

--- RPostgreSQL/src/RS-PQescape.c
+++ RPostgreSQL/src/RS-PQescape.c
@@ -6,7 +6,6 @@
 
 #include "RS-PostgreSQL.h"
 
-
 /* 
  * Adapter function to PQescapeStringConn()
  * This function should properly escape the string argument 
@@ -14,7 +13,6 @@
  * connection.
  * Note the single quote is not attached in the return val.
  */
-
 SEXP
 RS_PostgreSQL_escape(SEXP conHandle, SEXP preescapestring)
 {
@@ -34,4 +32,51 @@ RS_PostgreSQL_escape(SEXP conHandle, SEXP preescapestring)
     SET_STRING_ELT(output, 0, mkChar(escapedstring));
     return output;
 }
+
+/* 
+ * Adapter function to PQescapeByteaConn()
+ * This function should properly escape the raw argument 
+ * appropriately depending on connection.
+ * Note the single quote is not attached in the return val.
+ */
+SEXP
+RS_PostgreSQL_escape_bytea(SEXP conHandle, SEXP raw_data)
+{
+    S_EVALUATOR PGconn * my_connection;
+    RS_DBI_connection *con;
+    SEXP output;
+    size_t length;
+    const char *statement_cstr;
+    char *escapedstring;
+    size_t escaped_length;
+    con = RS_DBI_getConnection(conHandle);
+    my_connection = (PGconn *) con->drvConnection;
+    length = LENGTH(raw_data);
+    escapedstring = PQescapeByteaConn(my_connection, RAW(raw_data), length, &escaped_length);
+    PROTECT(output = allocVector(STRSXP, 1));
+    SET_STRING_ELT(output, 0, mkChar(escapedstring));
+    free(escapedstring);
+    UNPROTECT(1);
+    return output;
+}
+
+
+/* 
+ * Adapter function to PQunescapeBytea()
+ * This function should properly unescape the charactor representation
+ * of the binary data returned from PostgreSQL and return raw binary data.
+ */
+SEXP
+RS_PostgreSQL_unescape_bytea(SEXP escapedstring)
+{
+    SEXP output;
+    size_t raw_length;
+    unsigned char* rawbuffer;
+    rawbuffer = PQunescapeBytea(CHAR(STRING_ELT(escapedstring, 0)),  &raw_length);
+    output = allocVector(RAWSXP, raw_length);
+    memcpy(RAW(output), rawbuffer, raw_length);
+    free(rawbuffer);
+    return output;
+}
+
 
